95eb7c8ab2643332cd131ee6aadde94fdcbcd592
TheHulmaren <rbalsdldi@naver.com>
2023-05-05 00:04:20 +0900

    Add ProductController

---
+++ GCommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GCommerce.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public ProductController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var fetchedProducts = await _context.Products.ToListAsync();
+        return Ok(fetchedProducts);
+    }
+}
