54a897a557f0aed3178dfccd0b7c49569b29e197
TheHulmaren <rbalsdldi@naver.com>
2023-05-05 18:07:11 +0900

    feat: Add ServiceResponse model

---
+++ GCommerce/Shared/ServiceResponse.cs
@@ -0,0 +1,9 @@
+namespace GCommerce.Shared;
+
+public class ServiceResponse<T>
+{
+    public T? Data { get; set; }
+
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = string.Empty;
+}
