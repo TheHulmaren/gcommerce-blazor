cfad04daeb99cb532cac8ab3b5dfc94798b3b725
TheHulmaren <rbalsdldi@naver.com>
2023-05-05 18:06:05 +0900

    feat: Add injection on IProductService and update usings

--- GCommerce/Client/Program.cs
+++ GCommerce/Client/Program.cs
@@ -1,3 +1,6 @@
+global using System.Net.Http.Json;
+global using GCommerce.Shared;
+global using GCommerce.Client.Services.ProductServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GCommerce.Client;
@@ -8,5 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
     { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
