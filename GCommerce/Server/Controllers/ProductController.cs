3098224ec94af614d9888c01175aa4d98d756a64
TheHulmaren <rbalsdldi@naver.com>
2023-05-07 16:18:06 +0900

    feat: Add new controller functions

--- GCommerce/Server/Controllers/ProductController.cs
+++ GCommerce/Server/Controllers/ProductController.cs
@@ -20,4 +20,28 @@ public class ProductController : ControllerBase
         var serviceResponse = await _productService.GetProductsAsync();
         return Ok(serviceResponse);
     }
+
+    [HttpGet("{productId:int}")]
+    public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
+    {
+        var serviceResponse = await _productService.GetProductByIdAsync(productId);
+        if (serviceResponse.Success)
+        {
+            return Ok(serviceResponse);
+        }
+
+        return NotFound(serviceResponse);
+    }
+    
+    [HttpGet("category/{categorySlug}")]
+    public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsByCategorySlug(string categorySlug)
+    {
+        var serviceResponse = await _productService.GetProductsByCategorySlug(categorySlug);
+        if (serviceResponse.Success)
+        {
+            return Ok(serviceResponse);
+        }
+
+        return NotFound(serviceResponse);
+    }
 }
